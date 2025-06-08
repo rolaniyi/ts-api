43970ebdecc4fa42098fa527cd61e537e2fc594a
rolaniyi <59543540+rolaniyi@users.noreply.github.com>
2025-06-08 13:18:30 -0400

    Update base.py

--- ts/client/base.py
+++ ts/client/base.py
@@ -611,11 +611,9 @@ class BaseClient(ABC):
             "lastdate": lastdate,
             "sessiontemplate": sessiontemplate,
         }
-        
-        #header = {"Authorization": f"Bearer {self._access_token}"}
-        
-        #return self._get_request(url=url_endpoint, params=params), url_endpoint
-        return url_endpoint
+                
+        return self._get_request(url=url_endpoint, params=params) 
+
 
     def get_crypto_symbol_names(self) -> Response | Awaitable[Response]:
         """Fetch all crypto Symbol Names information."""
